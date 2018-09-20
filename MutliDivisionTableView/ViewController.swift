313b5b15c8c4db2d63b9d8b7f57ad056b1cefbcc
Viswa <viswanath3344@gmail.com>
2018-09-20 09:21:01 +0530

    Initial Commit

---
+++ MutliDivisionTableView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MutliDivisionTableView
+//
+//  Created by Ming-En Liu on 20/09/18.
+//  Copyright © 2018 Vedas labs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
